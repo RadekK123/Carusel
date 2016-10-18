5ac439489fbbe41c39d023f2714923287dc1af2e
Radek Knapkiewicz <radknap@gmail.com>
2016-10-18 16:46:49 +0200

    js current

--- js/firstjs.js
+++ js/firstjs.js
@@ -1,23 +1,6 @@
 $(document).ready(function () {
 
     var carusel = $('#carusel ul');
-    var caruselList = carusel.find('li');
-
-
-    function moveFirstSlide() {
-        var firstLi = carusel.find('li:first');
-        var lastLi = carusel.find('li:last');
-        lastLi.after(firstLi);
-        carusel.css({ marginLeft: '0px' });
-    }
-
-    function moveLastSlide() {
-        var firstLi = carusel.find('li:first');
-        var lastLi = carusel.find('li:last');
-        firstLi.before(lastLi);
-        carusel.css({ marginLeft: '-800px'});
-    }
-
     var leftArrow = $('#carusel .rectangle .glyphicon-menu-left');
     var rightArrow = $('#carusel .rectangle .glyphicon-menu-right');
 
@@ -29,11 +12,9 @@ $(document).ready(function () {
         var activeBall = $('.active');
         var nextBall = activeBall.next();
 
-        var Interval;
         if (!nextBall[0]) {
             
             var firstBall = $('.pagination .ball:first');
-            var lastBall = $('.pagination .ball:last');
 
             firstBall.removeClass('active');
             firstBall.click();
@@ -50,7 +31,6 @@ $(document).ready(function () {
         var prevBall = activeBall.prev();
         if (!prevBall[0]) {
             
-            var firstBall = $('.pagination .ball:first');
             var lastBall = $('.pagination .ball:last');
 
             lastBall.click();
